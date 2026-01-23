0f75dd1a6b1266b9cc780b754fd1046230cbc3a3
Rohan <rohantatipamul01@gmail.com>
2026-01-23 12:33:43 +0530

    Program 44 done

---
+++ Replace.java
@@ -0,0 +1,12 @@
+// Replace 0's with 1's
+public class Replace {
+    
+public static void main(String[] args)
+{
+int num = 706120678;
+String str = ""+num;
+System.out.println(Integer.parseInt(str.replace("0","1")));
+}
+
+}
+
